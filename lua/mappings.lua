455cd44d97c8cedc9005d51e5135161e32e02899
Moșneguțu Adrian-Ioan <adrianmosnegutuioan@yahoo.com>
2024-12-29 14:26:47 +0200

    add renaming capabilities

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -15,28 +15,33 @@ vim.keymap.set("n", "<leader>sn", ":Telescope notify<CR>", { desc = "Show Notifi
 -- Color Schemes
 vim.keymap.set("n", "<leader>th", ":Themery<CR>", { desc = "Change Theme" })
 
--- Buffer line
+-- Buffer Line
 vim.api.nvim_set_keymap(
-    "n",                           -- Normal mode
-    "<Tab>",                       -- Keybind
-    ":BufferLineCycleNext<CR>",    -- Command to go to the next tab
-    { noremap = true, silent = true } -- Options: no recursive mapping and silent
+	"n", -- Normal mode
+	"<Tab>", -- Keybind
+	":BufferLineCycleNext<CR>", -- Command to go to the next tab
+	{ noremap = true, silent = true } -- Options: no recursive mapping and silent
 )
 
+-- Incremental Rename
+vim.keymap.set("n", "<leader>rn", function()
+	return ":IncRename " .. vim.fn.expand("<cword>")
+end, { desc = "Rename Variable", expr = true })
+
 function CloseCurrentBufferAndSwitch()
-    -- Save the current buffer number
-    local current_buf = vim.api.nvim_get_current_buf()
+	-- Save the current buffer number
+	local current_buf = vim.api.nvim_get_current_buf()
 
-    -- Cycle to the next buffer
-    vim.cmd("BufferLineCycleNext")
+	-- Cycle to the next buffer
+	vim.cmd("BufferLineCycleNext")
 
-    -- Close the previously active buffer
-    vim.cmd("bdelete " .. current_buf)
+	-- Close the previously active buffer
+	vim.cmd("bdelete " .. current_buf)
 end
 
 vim.api.nvim_set_keymap(
-    "n",
-    "<Leader>x",
-    ":lua CloseCurrentBufferAndSwitch()<CR>",
-    { desc = "Close Tab", noremap = true, silent = true }
+	"n",
+	"<Leader>x",
+	":lua CloseCurrentBufferAndSwitch()<CR>",
+	{ desc = "Close Tab", noremap = true, silent = true }
 )
--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,42 +1,42 @@
 return {
-    {
-        "williamboman/mason.nvim",
-        config = function()
-            require("mason").setup({
-                ui = {
-                    border = "rounded",
-                },
-            })
-        end,
-    },
-    {
-        "williamboman/mason-lspconfig.nvim",
-        config = function()
-            require("mason-lspconfig").setup({
-                ensure_installed = { "lua_ls" },
-            })
-        end,
-    },
-    {
-        "neovim/nvim-lspconfig",
-        config = function()
-            local capabilities = require("cmp_nvim_lsp").default_capabilities()
-            local lspconfig = require("lspconfig")
+	{
+		"williamboman/mason.nvim",
+		config = function()
+			require("mason").setup({
+				ui = {
+					border = "rounded",
+				},
+			})
+		end,
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		config = function()
+			require("mason-lspconfig").setup({
+				ensure_installed = { "lua_ls" },
+			})
+		end,
+	},
+	{
+		"neovim/nvim-lspconfig",
+		config = function()
+			local capabilities = require("cmp_nvim_lsp").default_capabilities()
+			local lspconfig = require("lspconfig")
 
-            lspconfig.lua_ls.setup({
-                capabilities = capabilities,
-            })
-            lspconfig.clangd.setup({
-                capabilities = capabilities,
-            })
-            lspconfig.jdtls.setup({
-                capabilities = capabilities,
-            })
+			lspconfig.lua_ls.setup({ -- Lua
+				capabilities = capabilities,
+			})
+			lspconfig.clangd.setup({ -- C/C++
+				capabilities = capabilities,
+			})
+			lspconfig.jdtls.setup({ -- Java
+				capabilities = capabilities,
+			})
 
-            -- Customize hover documentation borders
-            vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, {
-                border = "rounded", -- Rounded borders
-            })
-        end,
-    },
+			-- Customize hover documentation borders
+			vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, {
+				border = "rounded", -- Rounded borders
+			})
+		end,
+	},
 }
