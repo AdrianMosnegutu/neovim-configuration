f513f802d2815c0f9f2a0a31a9aacdf4e5c8bf5f
Moșneguțu Adrian-Ioan <adrianmosnegutuioan@yahoo.com>
2024-12-30 20:25:40 +0200

    better file naming

--- lua/plugins/notifications.lua
+++ lua/plugins/notifications.lua
@@ -2,7 +2,7 @@ return {
 	"rcarriga/nvim-notify",
 	config = function()
 		local notify = require("notify")
-		notify.setup({ fps = 60, render = "wrapped-default", timeout = 3000 })
+		notify.setup({ fps = 60, render = "wrapped-compact", timeout = 3000 })
 		vim.notify = notify
 	end,
 }
